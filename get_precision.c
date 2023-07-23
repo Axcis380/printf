09cac34c993d030151848a5c28014d72e540a85c
halas98 <132351477+halas98@users.noreply.github.com>
2023-07-23 19:21:10 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,10 +1,4 @@
 #include "main.h"
-#include <stdbool.h> 
-
-bool is_digit(char c)
-{
-    return (c >= '0' && c <= '9');
-}
 
 /**
  * get_precision - Calculates the precision for printing
@@ -16,31 +10,32 @@ bool is_digit(char c)
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1;
-    int precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-    if (format[curr_i] != '.')
-        return precision;
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0;
-    curr_i++;
+	precision = 0;
 
-    if (format[curr_i] == '*')
-    {
-        precision = va_arg(list, int);
-        curr_i++;
-    }
-    else
-    {
-        while (is_digit(format[curr_i]))
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0';
-            curr_i++;
-        }
-    }
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    *i = curr_i - 1;
+	*i = curr_i - 1;
 
-    return precision;
+	return (precision);
 }
