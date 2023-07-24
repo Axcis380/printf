28b94b205ac7514a6c897bc5ecd2c88c2bd50314
halas98 <132351477+halas98@users.noreply.github.com>
2023-07-24 10:11:14 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef HOL_H
-#define HOL_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -114,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* HOL_H */
+#endif /* MAIN_H */
