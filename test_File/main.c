06f9931fbc8f6e9772ce78defdbbd44f8f0cf79a
halas98 <salahkalada4@gmail.com>
2023-07-25 06:35:50 -0700

    Printf

--- test_File/main.c
+++ test_File/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+/**
+  *
+  *
+  *
+  *
+  */
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
     int len;
@@ -34,6 +45,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
-
--- utils.c
+++ utils.c
@@ -8,9 +8,10 @@
  */
 int is_printable(char c)
 {
-if (c >= 32 && c < 127)
-return (1);
-return (0);
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
 }
 
 /**
@@ -22,15 +23,18 @@ return (0);
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-char map_to[] = "0123456789ABCDEF";
-/* The hexa format code is always 2 digits long */
-if (ascii_code < 0)
-ascii_code *= -1;
-buffer[i++] = '\\';
-buffer[i++] = 'x';
-buffer[i++] = map_to[ascii_code / 16];
-buffer[i] = map_to[ascii_code % 16];
-return (3);
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
 }
 
 /**
@@ -41,9 +45,10 @@ return (3);
  */
 int is_digit(char c)
 {
-if (c >= '0' && c <= '9')
-return (1);
-return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
 }
 
 /**
@@ -55,11 +60,12 @@ return (0);
  */
 long int convert_size_number(long int num, int size)
 {
-if (size == S_LONG)
-return (num);
-else if (size == S_SHORT)
-return ((short)num);
-return ((int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
 }
 
 /**
@@ -71,9 +77,10 @@ return ((int)num);
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-if (size == S_LONG)
-return (num);
-else if (size == S_SHORT)
-return ((unsigned short)num);
-return ((unsigned int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
 }
