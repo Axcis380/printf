7d81527a5bc89d3c45e13d5042194084eff483aa
halas98 <132351477+halas98@users.noreply.github.com>
2023-07-25 11:43:29 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -4,57 +4,38 @@
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: List of arguments.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1;
-    int precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-    if (format[curr_i] != '.')
-        return precision;
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0;
+	precision = 0;
 
-    // Move to the character after '.'
-    curr_i++;
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    // Check if precision is specified using '*'
-    if (format[curr_i] == '*')
-    {
-        curr_i++;
-        precision = va_arg(list, int);
-        if (precision < 0) // Handle negative values
-            precision = -1;
-    }
-    else
-    {
-        // Calculate the precision value from the format string
-        while (is_digit(format[curr_i]))
-        {
-            precision *= 10;
-            precision += format[curr_i] - '0';
-            curr_i++;
-        }
-    }
+	*i = curr_i - 1;
 
-    // Check for non-custom conversion specifiers
-    switch (format[curr_i])
-    {
-        case 'd':
-        case 'i':
-        case 'c':
-            // For %d, %i, %c, apply precision to limit the number of characters to print
-            break;
-        default:
-            // For other conversion specifiers, handle precision as needed
-            break;
-    }
-
-    *i = curr_i - 1;
-
-    return precision;
+	return (precision);
 }
-
